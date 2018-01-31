a00a41464330ed3d48e879ab711b156a13de41fb
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-31 12:35:19 -0800

    Completed React Components instructions.

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      found: false
+    };
+  }
+
+  onListItemEnter() {
+    this.setState({
+      found: true
+    });
+  }
+  onListItemLeave() {
+    this.setState({
+      found: false
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.found ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemEnter.bind(this)} onMouseOut={this.onListItemLeave.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+<div>
+  <h2>My Grocery List</h2>
+  <GroceryList items={['Apples', 'Oranges', 'Bananas']}/>
+</div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
